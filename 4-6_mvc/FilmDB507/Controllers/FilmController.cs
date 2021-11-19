45e93e8a27bc120e2e03f3601d5f8bcb1de6c349
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2021-11-20 00:37:05 +0100

    filmdb next tasks

---
+++ 4-6_mvc/FilmDB507/Controllers/FilmController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB.Controllers
+{
+    public class FilmController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
