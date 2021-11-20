efda8c3347b06e3c8af76263e6da88a5255c15af
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2021-11-20 17:14:14 +0100

    add FilmDB510

---
+++ 4-6_mvc/FilmDB510/Controllers/FilmController.cs
@@ -0,0 +1,81 @@
+﻿using FilmDB510.Logic;
+using FilmDB510.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB510.Controllers
+{
+    public class FilmController : Controller
+    {
+        public IActionResult Index()
+        {
+            var manager = new FilmManager();
+            var films = manager.GetFilms();
+
+            return View(films);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(FilmModel film)
+        {
+            var manager = new FilmManager();
+            manager.AddFilm(film);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Remove(int id)
+        {
+            var manager = new FilmManager();
+            var film = manager.GetFilm(id);
+            return View(film);
+        }
+
+        [HttpPost]
+        public IActionResult RemoveConfirm(int id)
+        {
+            var manager = new FilmManager();
+            try
+            {
+                manager.RemoveFilm(id);
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var manager = new FilmManager();
+            var film = manager.GetFilm(id);
+            return View(film);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(FilmModel film)
+        {
+            var manager = new FilmManager();
+            manager.UpdateFilm(film);
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 4-6_mvc/FilmDB510/FilmContext.cs
@@ -0,0 +1,19 @@
+﻿using FilmDB510.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB510
+{
+    public class FilmContext : DbContext
+    {
+        public DbSet<FilmModel> Films { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string con = @"Data Source=DESKTOP-55KJ8EL\SQLEXPRESS;Initial Catalog=FilmDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            optionsBuilder.UseSqlServer(con);
+        }
+    }
+}
