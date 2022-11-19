230d78d1292ce196e606d8cda24a9a0bd0a78ae3
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2022-11-19 14:26:10 +0100

    508 Chapter 4-6 Task 5,6

--- 4-6_mvc/Z508/FilmDB/Controllers/FilmController.cs
+++ 4-6_mvc/Z508/FilmDB/Controllers/FilmController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FilmDB.Logic;
+using FilmDB.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FilmDB.Controllers
 {
     public class FilmController : Controller
     {
+        FilmManager filmManager = new FilmManager();
         public IActionResult Index()
         {
+            FilmModel film = new FilmModel()
+            {
+                Name = "Rambo",
+                Year = 1985,
+            };
+            filmManager.AddFilm(film);
+
             return View();
         }
     }
---
+++ 4-6_mvc/Z508/FilmDB/Logic/FilmManager.cs
@@ -0,0 +1,54 @@
+﻿using FilmDB.Models;
+using System.Collections.Generic;
+
+namespace FilmDB.Logic
+{
+    public class FilmManager
+    {
+        public FilmManager AddFilm(FilmModel filmModel)
+        {
+            using (var context = new FilmContext())
+            {
+                context.Films.Add(filmModel);
+                context.SaveChanges();
+                //try
+                //{
+                //    context.Films.Add(filmModel);
+                //    context.SaveChanges();
+                //}
+                //catch (System.Exception e)
+                //{
+                //    filmModel.ID = 0;
+                //    context.Films.Add(filmModel);
+                //    context.SaveChanges();
+                //}
+            }
+            return this;
+        }
+
+        public FilmManager RemoveFilm(int id)
+        {
+            return this;
+        }
+
+        public FilmManager UpdateFilm(FilmModel filmModel)
+        {
+            return this;
+        }
+
+        public FilmManager ChangeTitle(int id, string newTitle)
+        {
+            return this;
+        }
+
+        public FilmManager GetFilm(int id)
+        {
+            return null;
+        }
+
+        public List<FilmModel> GetFilms()
+        {
+            return null;
+        }
+    }
+}
